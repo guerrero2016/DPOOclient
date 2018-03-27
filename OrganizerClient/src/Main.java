f853d0a42843c051ef59e5a71ff9a0282880175c
Aleix Baggerman <aleixdiazbaggerman@gmail.com>
2018-03-27 12:53:48 +0200

    [CORE] ProjectSelction, creation i info funcionant

--- OrganizerClient/src/Main.java
+++ OrganizerClient/src/Main.java
@@ -1,13 +1,12 @@
 import Controller.ProjectSelectionController;
-import View.ProjectInfoView;
 import View.ProjectSelectionView;
 
 public class Main {
 
     public static void main(String[] args) {
-        ProjectSelectionView projectSelectionView = new ProjectSelectionView();
-        ProjectSelectionController projectSelectionController = new ProjectSelectionController(projectSelectionView);
-        projectSelectionView.registerController(projectSelectionController);
+        //ProjectSelectionView projectSelectionView = new ProjectSelectionView();
+        //ProjectSelectionController projectSelectionController = new ProjectSelectionController(projectSelectionView);
+        //projectSelectionView.registerController(projectSelectionController);
     }
 
 
