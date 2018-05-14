983f465fb49ca7a2867ebf5bc84ad8e82b58738e
Aleix Baggerman <aleixdiazbaggerman@gmail.com>
2018-05-14 10:58:24 +0200

    [CORE] TaskSetCommunicator fixed.

--- OrganizerClient/src/Network/Communicators/TaskSetCommunicator.java
+++ OrganizerClient/src/Network/Communicators/TaskSetCommunicator.java
@@ -8,13 +8,16 @@ import Network.Communicable;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 
+/**
+ * Comunicador que s'encarrega de escoltar si algun usuari ha afegit o modificat alguna tasca.
+ */
 public class TaskSetCommunicator implements Communicable {
     @Override
     public void communicate(MainViewController controller, ObjectInputStream objectIn) {
         try {
             DataManager dataManager = DataManager.getSharedInstance();
             Task task = (Task) objectIn.readObject();
-            String categoryID = objectIn.readUTF();
+            String categoryID = objectIn.readObject().toString();
 
             dataManager.setTask(task, categoryID);
 
