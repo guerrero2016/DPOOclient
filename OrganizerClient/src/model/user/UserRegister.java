8a3eacc666a5b893a47f9029ebb9f88c6583cb78
Bernat Rovirosa Roca <bernat.rovirosa.2016@salleurl.edu>
2018-05-21 01:02:24 +0200

    [DOC] Model

--- OrganizerClient/src/model/user/UserRegister.java
+++ OrganizerClient/src/model/user/UserRegister.java
@@ -56,8 +56,11 @@ public class UserRegister extends User {
     }
 
     /**
-     *
-     * @return
+     * Funció encarregada de revisar si el registre és correcte.
+     * @return un codi depenent de l'error. <code>NAME_ERROR</code> si hi ha hagut error al nom. <code>EMAIL_ERROR</code>
+     *          si hi ha hagut error al email. <code>PASS_ERROR</code> si n'hi ha hagut a la contrasenya.
+     *          Si hi ha hagut error en més d'un camp, els errors es sumen; per exemple si hi ha error al nom i al email,
+     *          el codi d'error serà <code>NAME_ERROR</code> + <code>EMAIL_ERROR</code>.
      */
     private int checkSignIn() {
         int error = 0;
